93bdeabbdb59a355f9f002c884025946212b77c0
Mike Enochs <msenochs@umich.edu>
2019-11-27 18:29:15 -0500

    testimonial text fix and started recent listing component
    Noticed when I entered a rather large paragraph of text for a testimonial it overflowed the container (because I have a set 40vh for the div.row).  Tried multiple solutions to try to get the text to auto re-size with no luck.  Ended up applying font-size: 85% to the carousel-item which contains the testimonials we currently have.  I really don't want to turn off the 40vh, which would go back to the default of  height: auto for the container-fluid - it would fit the text at bigger font-sizes but the div itself would constantly change in size every transition. Will likely need a better solution long term.

--- frontend/src/components/customers/Testimonials.js
+++ frontend/src/components/customers/Testimonials.js
@@ -1,11 +1,11 @@
 import './Testimonials.css';
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import Markdown from 'markdown-to-jsx';
+// import Markdown from 'markdown-to-jsx';
 import Carousel from 'react-bootstrap/Carousel';
 import { connect } from 'react-redux';
 import { getTestimonials } from '../../actions/testimonial';
-import { render } from 'react-dom';
+// import { render } from 'react-dom';
 
 const Testimonials = ({
   getTestimonials,
@@ -22,7 +22,7 @@ const Testimonials = ({
       <div className='testimonials container-fluid'>
         <div className='testimonials row'>
           <Carousel className='text-center align-self-center px-3'>
-            {testimonials.map(testimonial => (
+            {testimonials.map((testimonial, textFit) => (
               <Carousel.Item key={testimonial._id}>
                 <i className='fas fa-quote-left'></i>
                 {testimonial.client_testimonial}
@@ -38,6 +38,28 @@ const Testimonials = ({
     </Fragment>
   );
 };
+
+//   return (
+//     <Fragment>
+//       <div className='testimonials container-fluid'>
+//         <div className='testimonials row'>
+//           <Carousel className='text-center align-self-center px-3'>
+//             {testimonials.map(testimonial => (
+//               <Carousel.Item key={testimonial._id}>
+//                 <i className='fas fa-quote-left'></i>
+//                 {testimonial.client_testimonial}
+//                 <i className='fas fa-quote-right'></i>
+//                 <footer className='blockquote-footer'>
+//                   <cite title='Source Title'>{testimonial.client_name}</cite>
+//                 </footer>
+//               </Carousel.Item>
+//             ))}
+//           </Carousel>
+//         </div>
+//       </div>
+//     </Fragment>
+//   );
+// };
 
 Testimonials.propTypes = {
   getTestimonials: PropTypes.func.isRequired,
--- frontend/src/components/listings/RecentListing.js
+++ frontend/src/components/listings/RecentListing.js
@@ -1,11 +1,38 @@
 import './RecentListing.css';
 import React, { Fragment } from 'react';
+import Longworth from '../../img/placeholders/longworth_cover.jpg';
 
 const RecentListing = () => {
   return (
     <Fragment>
       <div className='recent-listing col-12 col-md-8'>
-        Most Recent Listing Component
+        <div class='card text-center'>
+          <div class='card-header'>
+            <h3>Most Recent Listing</h3>
+          </div>
+          <img className='img-fluid' src={Longworth} alt='' />
+          <div class='card-body'>
+            <h5 class='card-title'>8031 Longworth Street Detroit, MI 48209</h5>
+            <a href='#' class='btn btn-primary'>
+              More Info
+            </a>
+          </div>
+        </div>
+
+        <form>
+          <div class='row'>
+            <div class='col'>
+              <input
+                type='text'
+                class='form-control'
+                placeholder='First name'
+              />
+            </div>
+            <div class='col'>
+              <input type='text' class='form-control' placeholder='Last name' />
+            </div>
+          </div>
+        </form>
       </div>
     </Fragment>
   );
