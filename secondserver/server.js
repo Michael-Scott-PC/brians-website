8e56a8774ffbf4e8fb6e10e1fcddca6a69de5db6
Mike Enochs <msenochs@umich.edu>
2019-12-17 11:38:50 -0500

    updated server.js in secondserver dir & updated package.json in root

--- secondserver/server.js
+++ secondserver/server.js
@@ -10,15 +10,15 @@ app.use(express.json({ extended: true }));
 app.use('/api/calculator', require('./routes/api/calculator'));
 // app.use('/api/news', require('./routes/api/news'));
 
-// Serve static assets in production
-// if (process.env.NODE_ENV === "production") {
-//     // Set static folder
-//     app.use(express.static("../frontend/build"));
+Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("../frontend/build"));
 
-//     app.get("*", (req, res) => {
-//       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//     });
-//   }
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
 
 const PORT = process.env.PORT || 5000;
 
