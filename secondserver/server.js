60fedf5ff397e245b1e3fd6a0954d26388dc0fb8
Mike Enochs <msenochs@umich.edu>
2019-12-20 11:28:31 -0500

    I'm about to start reverting commits

--- secondserver/server.js
+++ secondserver/server.js
@@ -1,25 +1,25 @@
-const express = require('express');
-// const path = require('path');
+// const express = require('express');
+// // const path = require('path');
 
-const app = express();
+// const app = express();
 
-// Init Middleware
-app.use(express.json({ extended: true }));
+// // Init Middleware
+// app.use(express.json({ extended: true }));
 
-// Define route
-app.use('/api/calculator', require('./routes/api/calculator'));
-// app.use('/api/news', require('./routes/api/news'));
+// // Define route
+// app.use('/api/calculator', require('./routes/api/calculator'));
+// // app.use('/api/news', require('./routes/api/news'));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('../frontend/build'));
+// // Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('../frontend/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+// app.listen(PORT, () => console.log(`server started on port ${PORT}`));
